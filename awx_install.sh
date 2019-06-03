98b07e8d2271657016ca933023abee1d0777ea84
Growdelan <34998212+growdelan@users.noreply.github.com>
2019-06-03 17:51:17 +0200

    Update awx_install.sh

--- awx_install.sh
+++ awx_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
+yum install python-pip ansible wget -y
 wget https://download.docker.com/linux/centos/docker-ce.repo -O /etc/yum.repos.d/docker-ce.repo
 yum install docker-ce docker-ce-cli containerd.io -y
 systemctl enable docker
 systemctl start docker
-yum install python-pip ansible -y
 pip install docker
 pip install docker-compose
 git clone https://github.com/ansible/awx.git
