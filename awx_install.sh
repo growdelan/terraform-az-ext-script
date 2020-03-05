c68607883b83642c896010ebeefe21183fb29b20
Growdelan <34998212+growdelan@users.noreply.github.com>
2020-03-05 18:44:38 +0100

    podniesienie pip

--- awx_install.sh
+++ awx_install.sh
@@ -4,10 +4,12 @@ yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.r
 yum install python-pip ansible wget git -y
 wget https://download.docker.com/linux/centos/docker-ce.repo -O /etc/yum.repos.d/docker-ce.repo
 yum install docker-ce docker-ce-cli containerd.io -y
-systemctl enable docker
-systemctl start docker
+yum install -y python-pip gcc python-devel
+pip install --upgrade pip
 pip install docker
 pip install docker-compose
+systemctl enable docker
+systemctl start docker
 git clone https://github.com/ansible/awx.git
 cd awx/installer/
 ansible-playbook -i inventory install.yml
